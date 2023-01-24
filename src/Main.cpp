62545a4449c0f0cb41c1dcc4c24de737b4e224f6
Dark <darkgrouptw@gmail.com>
2023-01-24 16:04:55 +0800

    砍掉多餘的 Defined

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
@@ -18,11 +17,6 @@ int main() {
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
