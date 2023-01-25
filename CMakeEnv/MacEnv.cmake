a9e232febaa3d1c811157d41b2c048641538aa33
Dark <darkgrouptw@gmail.com>
2023-01-25 23:45:20 +0800

    上傳 Mac 的版本

---
+++ CMakeEnv/MacEnv.cmake
@@ -0,0 +1,2 @@
+set(GLFW_PATH /opt/homebrew/Cellar/glfw/3.3.8)
+set(GLM_PATH /opt/homebrew/Cellar/glm/0.9.9.8)
--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,4 @@
 cd build
+cmake -S ../ -B ./ -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
 make clean
 make
