5932fb12f12f678cda3aeeecda36f222f4ec6bd9
Dark <darkgrouptw@gmail.com>
2023-04-19 23:24:44 +0800

    Update version

--- CMakeEnv/WindowEnv.cmake
+++ CMakeEnv/WindowEnv.cmake
@@ -1,3 +1,3 @@
-set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.236.0)
+set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.243.0)
 set(GLFW_PATH D:/ExternalLibrary/glfw-3.3.8.bin.WIN64)
 set(GLM_PATH D:/ExternalLibrary/glm-0.9.9.8/glm)
