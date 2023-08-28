f14b411cc2982c5ea1d3260c6853efe8156ba73e
Dark <darkgrouptw@gmail.com>
2023-08-28 23:37:00 +0800

    更改位置

--- src/Core/Components/Scene/SceneManager.cpp
+++ src/Core/Components/Scene/SceneManager.cpp
@@ -12,7 +12,12 @@ SceneManager::~SceneManager()
 
 void SceneManager::LoadScene(string pSceneName)
 {
-
+	pSceneName = Common::GetResourcePath(pSceneName);
+#if USE_ASSIMP
+	loader.LoadScene(pSceneName);
+#else
+	throw runtime_error("NotImplemented other way to load scene");
+#endif
 }
 #pragma endregion
 #pragma region Private
