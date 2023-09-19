a45eb2b4e741afbdf06e99e63f91c760b5cf5c88
Dark <darkgrouptw@gmail.com>
2023-09-19 22:38:17 +0800

    Update vulkan sdk to 261

--- CMakeEnv/WindowEnv.cmake
+++ CMakeEnv/WindowEnv.cmake
@@ -1,3 +1,3 @@
-set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.250.0)
+set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.261.1)
 set(GLFW_PATH D:/ExternalLibrary/glfw-3.3.8.bin.WIN64)
 set(Assimp_SDK_PATH D:/ExternalLibrary/Assimp)
