3d2ecff3f50eb8bb9b5b6ce8c2360211efea3057
Dark <darkgrouptw@gmail.com>
2024-03-21 00:07:36 +0800

    Update Assimp Version

--- CMakeEnv/MacEnv.cmake
+++ CMakeEnv/MacEnv.cmake
@@ -1,2 +1,2 @@
 set(GLFW_PATH /opt/homebrew/Cellar/glfw/3.3.9)
-set(Assimp_SDK_PATH /opt/homebrew/Cellar/assimp/5.2.5)
+set(Assimp_SDK_PATH /opt/homebrew/Cellar/assimp/5.3.1)
--- CMakeEnv/WindowEnv.cmake
+++ CMakeEnv/WindowEnv.cmake
@@ -1,3 +1,3 @@
 set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.275.0)
 set(GLFW_PATH D:/ExternalLibrary/glfw-3.3.9.bin.WIN64)
-set(Assimp_SDK_PATH D:/ExternalLibrary/Assimp)
+set(Assimp_SDK_PATH D:/ExternalLibrary/Assimp-5.3.1)
